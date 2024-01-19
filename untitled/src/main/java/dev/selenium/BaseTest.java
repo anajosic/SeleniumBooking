04aef5364352dacf4a20033b86473d62386c5541
anajosic <156595491+anajosic@users.noreply.github.com>
2024-01-19 09:02:55 +0100

    Base Test added some methods
    sda

--- untitled/src/main/java/dev/selenium/BaseTest.java
+++ untitled/src/main/java/dev/selenium/BaseTest.java
@@ -1,4 +1,40 @@
 package dev.selenium;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.grid.Main;
+import org.openqa.selenium.net.PortProber;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BaseTest {
+     protected WebDriver driver;
+    protected WebDriverWait wait;
+    protected File driverPath;
+    protected File browserPath;
+
+    public WebElement getLocatedElement(WebDriver driver, By by) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        return wait.until(d -> driver.findElement(by));
+    }
+
+    protected ChromeDriver startChromeDriver() {
+        ChromeOptions options = new ChromeOptions();
+        options.setImplicitWaitTimeout(Duration.ofSeconds(1));
+        return startChromeDriver(options);
+    }
+
+    protected ChromeDriver startChromeDriver(ChromeOptions options) {
+        driver = new ChromeDriver(options);
+        return (ChromeDriver) driver;
+    }
+
 }
